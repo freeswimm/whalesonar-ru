488fcf9bf3d14447b4bbc0d3fb5fe63f54512a62
Den Maslyukov <freeswimm@gmail.com>
2017-06-22 15:19:50 -0500

    v1.0.2 (b)
    -  Анимация голосования в виде воздушных шаров(баллонов), при клике по баллону переход на пост автора
    - Выделение баллона с автором, если он совпадает с текущим пользователем
    - Новые звуки для дельфтнов и косаток с более плавным звучанием
    -  Переключатель, чтобы убирать все надписи с баллонами
    - Новый фон
    - Кнопка для сворачивания боковой панели
    - Багфикс

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 $(function () {
     
-    var big_whale_sound = document.getElementById("small_whale");
-    var small_whale_sound = document.getElementById("small_whale");
-    var dolphin_sound = document.getElementById("dolphin");
-    var orca_sound = document.getElementById("orca");
+       
+    var whale_sounds = [document.getElementById("whale1")];
+    var dolphin_sounds = [document.getElementById("dolphin1"), document.getElementById("dolphin2"), document.getElementById("dolphin3")];
+    var orca_sounds = [document.getElementById("orca1"), document.getElementById("orca2")];
+   
     
+    // фоны для шариков
+    var balloon_bg = ['blue-bg', 'green-bg', 'pink-bg', 'yellow-bg', 'orange-bg'];
+    
+    /* -----ДЕБАГ----- */    
     // режим отладки
     var debug = true;
-    
+    // останавливает кита 
+    var stop_mode = false;
+    var show_only = '';        
+    /* ---end дебаг ---*/
+        
     // конфигурация steem/golos
 
     var MODE = 'golos';
@@ -17,18 +26,18 @@ $(function () {
     // SP по умолчанию
     var DEFAULT_SP = 10000;
     // время действия всех кук в днях
-    var COOKIE_EXPIRES = 30;    
+    var COOKIE_EXPIRES = 90;    
     // черный список по умолчанию
     var BLACK_LIST = 'cheetah';
-    
+   
     // предустановки для STEEM / GOLOS
-    if(MODE == 'golos'){
+    if(MODE == 'golos'){ 
         steem.config.set('websocket', 'wss://ws.golos.io');
         var DOMAIN = 'https://golos.io/';
     } else {
         var DOMAIN = 'https://steemit.com/';
     }
-
+            
   /* Заполнить значения полей из cookie */
    
     if((Cookies.get('name') !== 'null') && (Cookies.get('name') !== undefined)){
@@ -54,13 +63,19 @@ $(function () {
     } else if((Cookies.get('sound_on') === 'off') || (Cookies.get('sound_on') === undefined)){        
          $('#sound_on').attr('checked', false);
     }
-    // состояние чекбоксов по кукам
+    
+    if(Cookies.get('balloon_on') === 'on'){
+       $('#balloon_on').attr('checked', true);
+    } else if((Cookies.get('balloon_on') === 'off') || (Cookies.get('balloon_on') === undefined)){        
+         $('#balloon_on').attr('checked', false);
+    }
+    
     if((Cookies.get('bg_on') === 'on') || (Cookies.get('bg_on') === undefined)){
        $('#bg_on').attr('checked', true);       
        $('body').addClass('bg-image');
     } else if((Cookies.get('bg_on') === 'off')){        
        $('#bg_on').attr('checked', false);
-       $('body').addClass('bg-gradient');
+       $('body').addClass('bg-image2');
     }
 
     /* События  */
@@ -86,7 +101,7 @@ $(function () {
 
     // показать/скрыть фон
     $('#bg_on').on('change', function(){
-        $('body').toggleClass('bg-gradient');
+        $('body').toggleClass('bg-image2');
         $('body').toggleClass('bg-image');
         
         if($('#bg_on').is(':checked')){
@@ -95,6 +110,18 @@ $(function () {
             Cookies.set('bg_on', 'off', {expires: COOKIE_EXPIRES});
         }    
     });
+    // показать/скрыть шары
+    $('#balloon_on').on('change', function(){
+        
+        if($('#balloon_on').is(':checked')){
+            $('.switch-balloon').removeClass('hidden');  
+            Cookies.set('balloon_on', 'on', {expires: COOKIE_EXPIRES});            
+        } else { 
+            $('.switch-balloon').addClass('hidden'); 
+            Cookies.set('balloon_on', 'off', {expires: COOKIE_EXPIRES});             
+        }    
+    });
+    
     // запомнить состояние звуков
     $('#sound_on').on('change', function(){       
         if($('#sound_on').is(':checked')){
@@ -103,7 +130,23 @@ $(function () {
             Cookies.set('sound_on', 'off', {expires: COOKIE_EXPIRES});
         }       
     });
-   
+    
+    // клик по шарику -> открыть пост автора
+    $('body').on('click', '.author-link-overlay', function(){
+        var link = $(this).data('link');
+        if(link !== ''){
+            window.open(link);
+        }        
+    });
+    
+    // свернуть боковую панель
+    $('#minimize').click(function (e) {
+        $(this).toggleClass('rotate-180');
+
+        //containerHeight(); // recalculate page height
+
+        $('.settings-box .wrapper').slideToggle(500, function(){$('#minimize').attr('style', 'display: block !important;');});
+    });
     
     /* Работа с транзакциями GOLOS/STEEM   */
     steem.api.getDynamicGlobalProperties(function (err, steem_data) {
@@ -115,32 +158,35 @@ $(function () {
                     operations.forEach(function (operation) {
                         // если это апвоут и не сущность из блеклиста
                         if ((operation.voter !== undefined) && (BLACK_LIST.indexOf(operation.voter) === -1)) {
-                            //_d(operation);
+                           // _d(operation);
                             // проверить акк голосующего на силу 
                             steem.api.getAccounts([operation.voter], function (err, account) {
                                 
-                                if (err === null) {
+                                if ((err === null)) {
                                     //var reputation = steem.formatter.reputation(result[0].reputation);
                                     // получить СГ аккаунта
                                     var SP = getSteemPower(steem_data, account);
-                              
-                                    if (SP >= $('#min_sp').val()*1) {
-                                       
-                                        var vote_data = {
-                                            voter: account[0].name,
-                                            author: operation.author,
-                                            link: DOMAIN+'@'+account[0].name,
-                                            voter_id: 'v'+account[0].id, // убрать возможные точки из id
-                                            sp: SP,
-                                            ava: getAvatar(account),
-                                            weight: operation.weight / 100,
-                                            timestart: Math.floor(Date.now() / 1000),
-                                            sound: false,
-                                            species: '',
-                                            gests: account[0].vesting_shares.split(' ')[0]
-                                        };
-                                        // выпустить кита на экран )
-                                        releaseWhale(vote_data);
+                                                                        
+                                    if((show_only === '') || (show_only === account[0].name)){  // DEBUG row -> проверка показа кита для отладочного режима
+                                        if (SP >= $('#min_sp').val()*1) {
+
+                                            var vote_data = {
+                                                voter: account[0].name,
+                                                author: operation.author,
+                                                permlink: DOMAIN+'@'+operation.author+'/'+operation.permlink,
+                                                link: DOMAIN+'@'+account[0].name,
+                                                voter_id: 'v'+account[0].id, // убрать возможные точки из id
+                                                sp: SP,
+                                                ava: getAvatar(account),
+                                                weight: operation.weight / 100,
+                                                timestart: Math.floor(Date.now() / 1000),
+                                                sound: false,
+                                                species: '',
+                                                gests: account[0].vesting_shares.split(' ')[0]
+                                            };
+                                            // выпустить кита на экран )
+                                            releaseWhale(vote_data);
+                                        }
                                     }
                                 } else {
                                     console.log('Error! Cant get account:', err);
@@ -194,13 +240,12 @@ $(function () {
         // создать животное       
         createWhale(whale_data);
         
-        // воспроизвести звук
+        // воспроизвести звук спустя 1 сек
         if (whale_data.sound && $('#sound_on').is(':checked')) {
-            setTimeout(function () {
-                whale_data.sound.play();
+            setTimeout(function () {               
+               whale_data.sound.play();
             }, 1000);
         }
-        //$("#bawl").play();
     }
     
     // создать анимацию (кит/орка/дельфин)
@@ -209,7 +254,7 @@ $(function () {
         if(data.species === '') { return false;}
         
         var params = {pic: '', alt:'', width: 0};
-        
+         
         switch(data.species){
             case 'dolphin':
                 params.pic = './img/dolphin.gif';
@@ -231,37 +276,64 @@ $(function () {
                 params.alt = 'Синий кит';
                 params.width = '280px';
                 break;
-        }
-       
-       // если животное с таким же ID еще на странице
-        if($('*').is('#'+data.voter_id)) {
-            // удалить предыдущий LIFE_INTERVAL
-            clearTimeout($('#'+data.voter_id).data('clear_id'));   
-           
-            // выставить новый
-            var timeoutID = setTimeout(function(){
-                $('#'+data.voter_id).remove();
-                // удалить кейфрейм с поведением
-                $('#keyframe_'+data.voter_id).remove();
-            }, LIFE_INTERVAL*1000);
+        }       
+        
+        // фон балона
+        var author_bg = (data.author === $('#name').val()) ? 'burn-bg' : balloon_bg[getRand(0,4)]; 
             
-            // изменить атрибут с ID интервала
-            $('#'+data.voter_id).data('clear_id', timeoutID);  
+        // если животное с таким же ID еще на странице
+        if($('*').is('#'+data.voter_id)) {                        
             
-        } else {
+            // изменить имя автора в баллоне
+            $('#'+data.voter_id+' .author-balloon span').text(data.author);
+            // ссылку на страницы автора
+            $('#'+data.voter_id+' .author-link-overlay').data('link', data.permlink);
+            // СГ в балоне
+            $('#'+data.voter_id+' .sp-title span').text(data.weight+"%");
+            // цвет балона 
+            $('#'+data.voter_id+' .author-balloon').removeClass().addClass('author-balloon').addClass('is-right-side').addClass(author_bg);
+            //$('#'+data.voter_id+' .author-balloon').addClass(author_bg);
+        } else {                                    
+             
             // убрать животное через промежуток времени LIFE_INTERVAL
             var timeoutID = setTimeout(function(){
-                $('#'+data.voter_id).remove();
                 
-                // удалить кейфрейм с поведением
-                $('#keyframe_'+data.voter_id).remove();
+                if(!stop_mode){ // DEBUG row -> не удалять стили в режиме stop_mode
+                    $('#'+data.voter_id).remove();
+                
+                    // удалить кейфрейм с поведением
+                    $('#keyframe_'+data.voter_id).remove();
+                }
+                
             }, LIFE_INTERVAL*1000);
             
-            // добавить кейфрейм с поведением для животного
-            $("<style id='keyframe_"+data.voter_id+"' type='text/css'>"+getLocus(data.voter_id)+" </style>").appendTo("head");
+            
+            if(!stop_mode){ // DEBUG row -> остановить кита
+                // добавить кейфрейм с поведением для животного
+                $("<style id='keyframe_"+data.voter_id+"' type='text/css'>"+getLocus(data.voter_id)+" </style>").appendTo("head");
+            } else {
+                // DEBUG row -> остановить кита
+                $("<style id='keyframe_"+data.voter_id+"' type='text/css'> #"+data.voter_id+"{top: 60%; left: 65%;} </style>").appendTo("head");
+            }          
+            
+            // фон тайтла в зав-ти от текущего куратора
+            var voter_bg = (data.voter === $('#name').val()) ? 'burn-bg' : 'blue-bg'; 
+            var balloon_on = ($('#balloon_on').is(':checked')) ? 'switch-balloon' : 'switch-balloon hidden';
+        
             // добавить контейнер с изображением на страницу
-            var bg = (data.voter === $('#name').val()) ? 'burn-bg' : 'blue-bg';
-            var container = "<div class='creature' id='"+data.voter_id+"' data-clear_id='"+timeoutID+"' style='animation:swim_"+data.voter_id+" "+LIFE_INTERVAL+"s infinite linear;'><a href='"+data.link+"' target='_blank'><img src='"+params.pic+"' alt='"+params.alt+"' width='"+params.width+"'/></a><span class='voter-title is-right-side "+bg+"'>"+data.voter+"</span></div>";
+            var container = "<div class='creature' id='"+data.voter_id+"' data-clear_id='"+timeoutID+"' style='animation:swim_"+data.voter_id+" "+LIFE_INTERVAL+"s infinite linear;'>\n\
+                             <a href='"+data.link+"'  target='_blank'>\n\
+                             <img src='"+params.pic+"' alt='"+params.alt+"' width='"+params.width+"'/>\n\
+                             </a>\n\
+                             <span class='voter-title is-right-side "+voter_bg+" "+balloon_on+"'>"+data.voter+"</span>\n\
+                                <svg class='title-curve-line "+balloon_on+"'>\n\
+                                  <path fill='none' stroke='#99b' stroke-width='1' d='M30,0 Q30,50 60,60' />\n\
+                                </svg>\n\
+                             <span class='author-balloon is-right-side "+author_bg+" "+balloon_on+"'><span>"+data.author+"</span></span>\n\
+                             <div class='sp-title is-right-side "+balloon_on+"'><span>"+data.weight+"%</span></div>\n\
+                             <span class='ballon-nippel "+balloon_on+"'></span>\n\
+                             <div class='author-link-overlay "+balloon_on+"' data-link='"+data.permlink+"'></div>\n\
+                            </div>";
             $('body').append(container);
         }              
     }
@@ -273,15 +345,15 @@ $(function () {
         gests = (data.gests*1) / 1000000;
         
         if ((gests >= 10) && (gests < 100)) {
-            data.sound = dolphin_sound;
+            data.sound = dolphin_sounds[getRand(0,dolphin_sounds.length-1)];
             data.species = 'dolphin';
         }
         if ((gests >= 100) && (gests < 1000)) {
-            data.sound = orca_sound;
+            data.sound = orca_sounds[getRand(0,orca_sounds.length-1)];
             data.species = 'orca';
         }
         if ((gests >= 1000) && (gests < 3000)) {
-            data.sound = small_whale_sound;
+            data.sound = whale_sounds[getRand(0,whale_sounds.length-1)];
             data.species = 'whale';
         }
         if (gests > 3000) {
@@ -326,4 +398,6 @@ $(function () {
             console.log(param);
         }
     }
+    
+    
 });
